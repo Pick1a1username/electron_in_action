1776ae68aad26d6d7bf342e9973f18dbc8756675
Pick1a1username <xscreensaver@outlook.com>
2020-05-03 12:35:24 +0900

    20200503_2

---
+++ chapter6/firesale/app/renderer.js
@@ -0,0 +1,62 @@
+const path = require('path');
+
+const { ipcRenderer, remote } = require('electron');
+const marked = require('marked');
+
+const mainProcess = remote.require('./main.js');
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
+
+const currentWindow = remote.getCurrentWindow();
+
+let filePath = null;
+let originalContent = '';
+
+const renderMarkdownToHtml = (markdown) => {
+    htmlView.innerHTML = marked(markdown, { sanitize: true });
+};
+
+const updateUserInterface = (isEdited) => {
+    let title = 'Fire Sale';
+    
+    if (filePath) title = `${path.basename(filePath)} - ${title}`;
+    if (isEdited) title = `${title} (Edited)`;
+
+    currentWindow.setTitle(title);
+    currentWindow.setDocumentEdited(isEdited);
+
+    saveMarkdownButton.disabled = !isEdited;
+    revertButton.disabled = !isEdited;
+};
+
+markdownView.addEventListener('keyup', (event) => {
+    const currentContent = event.target.value;
+    renderMarkdownToHtml(currentContent);
+    updateUserInterface(currentContent !== originalContent);
+});
+
+openFileButton.addEventListener('click', () => {
+    mainProcess.getFileFromUser(currentWindow);
+});
+
+newFileButton.addEventListener('click', () => {
+    mainProcess.createWindow();
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+    filePath = file;
+    originalContent = content;
+
+    markdownView.value = content;
+    renderMarkdownToHtml(content);
+
+    updateUserInterface();
+});
