80f933c51db009d8fe982523ae7f75d62e314ce7
Pick1a1username <xscreensaver@outlook.com>
2020-05-06 12:23:52 +0900

    20200506_4

--- chapter10/clipmaster/app/main.js
+++ chapter10/clipmaster/app/main.js
@@ -1,4 +1,4 @@
-const { globalShortcut } = require('electron');
+const { globalShortcut, Menu } = require('electron');
 const { menubar } = require('menubar');
 
 // https://github.com/maxogden/menubar
@@ -12,8 +12,20 @@ const mb = menubar({
     preloadWindow: true // https://github.com/maxogden/menubar/issues/65#issuecomment-141020275
 });
 
+const secondaryMenu = Menu.buildFromTemplate([
+    {
+        label: 'Quit',
+        click() { mb.app.quit(); },
+        accelerator: 'CommandOrControl+Q'
+    },
+]);
+
 mb.on('ready', () => {
     console.log('Application is ready.');
+
+    mb.tray.on('right-click', () => {
+        mb.tray.popUpContextMenu(secondaryMenu);
+    });
 
     const createClipping = globalShortcut.register('CommandOrControl+!', () => {
         mb.window.webContents.send('create-new-clipping');
