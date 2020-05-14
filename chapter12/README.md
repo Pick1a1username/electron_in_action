40bc526dc52b56af3eafc3ba3e62e70d842aa543
Pick1a1username <xscreensaver@outlook.com>
2020-05-15 08:18:37 +0900

    20200515_0

---
+++ chapter12/README.md
@@ -0,0 +1,5 @@
+# Chapter 11-2
+
+This directory makes use of `electron-webpack`. Refer to the following link for more details:
+
+https://webpack.electron.build/
---
+++ chapter12/jetsetter/src/renderer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+import Application from './components/Application';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Application />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
