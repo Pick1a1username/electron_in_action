ff1936440bb10dfe9101c88177ad9b1908a4be16
Pick1a1username <xscreensaver@outlook.com>
2020-05-16 17:21:06 +0900

    20200516_2

--- chapter12/jetsetter/src/database.js
+++ chapter12/jetsetter/src/database.js
@@ -1,10 +1,17 @@
+import * as path from 'path';
+
+// https://stackoverflow.com/questions/47957479/how-to-use-electrons-app-getpath-to-store-data
+import { remote } from 'electron';
 import 'sqlite3';
 import knex from 'knex';
 
 const database = knex({
     client: 'sqlite3',
     connection: {
-        filename: './db.sqlite'
+        filename: path.join(
+            remote.app.getPath('userData'),
+            'jetsetter-items.sqlite'
+        )
     },
     useNullAsDefault: true
 });
